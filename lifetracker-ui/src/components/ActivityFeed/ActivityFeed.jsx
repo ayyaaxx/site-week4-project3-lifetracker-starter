5da25cb943756c35c8ad11702ebfe84e68ee57e8
Aya Elfettahi <aelfettahi@aelfett-ltmzept.internal.salesforce.com>
2023-07-07 17:16:57 -0700

    finished with css

--- lifetracker-ui/src/components/ActivityFeed/ActivityFeed.jsx
+++ lifetracker-ui/src/components/ActivityFeed/ActivityFeed.jsx
@@ -1,34 +1,46 @@
-import "./ActivityFeed.css"
-import React from 'react'
+import "./ActivityFeed.css";
+import React from 'react';
 import { Link } from "react-router-dom";
 import ActivityHero from "../ActivityHero/ActivityHero";
 
 const ActivityFeed = () => {
   return (
-    <div className = "activity-feed">
+    <div className="activity-feed">
       <ActivityHero/>
-      <button>
-        Add Excercise 
-      </button>
-      <button>
-        <Link to="/sleep">Log Sleep</Link>
-    </button>
-      <button>
-      <Link to="/nutrition">Record Nutrition</Link> 
-      </button>
-      <div className = "total-excercise">
-      <h2>Total Excercise Minutes</h2>
+      <div className="button">
+        <button>
+          <Link to="/activity">Add Exercise</Link>
+        </button>
+        <button>
+          <Link to="/sleep">Log Sleep</Link>
+        </button>
+        <button>
+          <Link to="/nutrition">Record Nutrition</Link> 
+        </button>
       </div>
-      <div className = "hour-sleep">
-      <h2>Average Hours of Sleep</h2>
+      <div className="activity-line">
+        <div className="total-exercise">
+          <h2>Total Exercise Minutes</h2>
+          <div className="activity-card">
+            <p>Exercise Minutes:</p>
+          </div>
+        </div>
+        <div className="hour-sleep">
+          <h2>Average Hours of Sleep</h2>
+          <div className="activity-card">
+            <p>Hours of Sleep:</p>
+          </div>
+        </div>
+        <div className="avg-daily">
+          <h2>Average Daily Calories</h2>
+          <div className="activity-card">
+            <p>Daily Categories:</p>
+          </div>
+        </div>
       </div>
-      <div className = "avg-daily">
-      <h2>Average Daily Calories</h2>
-      </div>
-
-      
     </div>
   )
 }
 
-export default ActivityFeed
+export default ActivityFeed;
+
